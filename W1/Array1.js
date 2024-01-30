e57cb9b370839e9f041a64c8b589d0e30ee97420
EmmaZhang <emmazxd@gmail.com>
2024-01-30 16:06:44 +0800

    Q8 commit

--- W1/Array1.js
+++ W1/Array1.js
@@ -257,8 +257,18 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
-console.log(reverseString("rockstar"));
+// console.log(reverseString("rockstar"));
 
 // 8.Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
+const longestStringInArray = function (arrOfStrings) {
+  let longestString = arrOfStrings[0].length;
+  for (let i = 1; i < arrOfStrings.length; i++) {
+    if (arrOfStrings[i].length > longestString) {
+      longestString = arrOfStrings[i].length;
+    }
+  }
+  return longestString;
+};
+console.log(longestStringInArray(["a", "ab", "bcdde"]));
 
 // 9.Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
