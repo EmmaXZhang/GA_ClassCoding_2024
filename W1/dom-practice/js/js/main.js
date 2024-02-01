77f3705b0f85c1d1c7bf93dd48b41e41d8562977
EmmaZhang <emmazxd@gmail.com>
2024-02-01 09:58:37 +0800

    updae dom js

---
+++ W1/dom-practice/js/js/main.js
@@ -0,0 +1,6 @@
+const titleEl = document.getElementById("title");
+
+const pEl = document.querySelector(".cool");
+
+pEl.innerText = "Comment for Today";
+pEl.innerHTML = "comments for <strong>Today</strong>";
