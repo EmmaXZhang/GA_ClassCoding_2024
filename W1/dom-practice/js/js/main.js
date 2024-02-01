95a905396451d91f21ae4a9739bab4eec74b1df3
EmmaZhang <emmazxd@gmail.com>
2024-02-01 10:18:16 +0800

    html update

--- W1/dom-practice/js/js/main.js
+++ W1/dom-practice/js/js/main.js
@@ -1,6 +1,12 @@
 const titleEl = document.getElementById("title");
+titleEl.style.textAlign = "center";
 
 const pEl = document.querySelector(".cool");
 
 pEl.innerText = "Comment for Today";
 pEl.innerHTML = "comments for <strong>Today</strong>";
+
+pEl.style.color = "red";
+
+const linkEl = document.querySelector("a");
+linkEl.setAttribute("href", "https://www.google.com");
