3d9d83c785a738702c352bc505efb8a1ded8c0ee
EmmaZhang <emmazxd@gmail.com>
2024-02-02 08:46:35 +0800

    state and constants

--- W1/Dice_Game/js/pig.js
+++ W1/Dice_Game/js/pig.js
@@ -1,9 +1,36 @@
 /*----- constants -----*/
+const SIDES = {
+  1: "&#x2680;",
+  2: "&#x2681;",
+  3: "&#x2682;",
+  4: "&#x2683;",
+  5: "&#x2684;",
+  6: "&#x2685;",
+};
 
 /*----- state variables -----*/
-
+//what you want to keep track
+const state = {
+  player: "",
+  totalPoints: {
+    player1: 0,
+    player2: 0,
+  },
+  points: 0,
+  rolls: [],
+};
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+const init = function () {
+  state.player = "player1";
+  state.totalPoints.player1 = 0;
+  state.totalPoints.player2 = 0;
+  state.points = 0;
+  state.rolls.length = 0;
+};
+
+init();
+console.log(state);
