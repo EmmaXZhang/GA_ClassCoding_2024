628190fa54ddf77af07630d5574342af80cfe947
EmmaZhang <emmazxd@gmail.com>
2024-02-06 13:11:18 +0800

    callback_lab Q3 commit

--- W2/JS_Callback_Lab.js
+++ W2/JS_Callback_Lab.js
@@ -43,7 +43,28 @@ function sortByLength(a, b) {
 // named longWords that includes only the words with 7 or more
 // characters
 
-const longWords = words.filter(/* write an anonymous inline function here */);
+const longWords = words.filter((word) => word.length >= 7);
+console.log(longWords);
 
 // Check that logging longWords outputs
 // ["lengthy", "delicious"]
+
+// Exercise 4
+// Code a forEach method:
+// 		1. Write a function named forEach.
+//		2. The forEach function accepts two args, an array & a callback.
+//		3. Code the forEach method such that it iterates over each element in the array arg (use a for loop).
+//		4. For each iteration, invoke the callback arg, passing to it, the element and the index of the element.
+
+// Test with this array
+const colors = ["red", "green", "blue", "purple"];
+// and this callback
+function log(elem, idx) {
+  console.log(`Index: ${idx} / Element Value: ${elem}`);
+}
+
+// calling forEach(colors, log) should resulting in this output:
+// Index: 0 / Element Value: red
+// Index: 1 / Element Value: green
+// Index: 2 / Element Value: blue
+// Index: 3 / Element Value: purple
