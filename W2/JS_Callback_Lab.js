8a976e16a71ba60f7993ca14d5c30222787c8043
EmmaZhang <emmazxd@gmail.com>
2024-02-06 13:31:30 +0800

    callbacl_lab Q5

--- W2/JS_Callback_Lab.js
+++ W2/JS_Callback_Lab.js
@@ -34,6 +34,7 @@ wordArr.sort((a, b) => a[1] - b[1]); //sort their value, key =0, value=1
 
 //solution 2
 words.sort(sortByLength);
+
 function sortByLength(a, b) {
   return a.length - b.length;
 }
@@ -76,3 +77,52 @@ forEach(colors, log);
 // Index: 1 / Element Value: green
 // Index: 2 / Element Value: blue
 // Index: 3 / Element Value: purple
+
+// Exercise 5
+
+function step1(cb) {
+  setTimeout(function () {
+    console.log("STEP 1 COMPLETE");
+    cb();
+  }, 750);
+}
+
+function step2(cb) {
+  setTimeout(function () {
+    console.log("STEP 2 COMPLETE");
+    cb();
+  }, 500);
+}
+
+function step3(cb) {
+  setTimeout(function () {
+    console.log("STEP 3 COMPLETE");
+    cb();
+  }, 250);
+}
+
+/*
+  The above functions are working asynchronous functions - DO NOT
+  change any of their code. They are what we call "black boxes"
+  because we do not need to know anything that goes on inside of them.
+  Each of the three functions accept a single argument - a callback function.
+  Write the code that invokes the three functions such that the output in the console will be:
+  
+  STEP 1 COMPLETE
+  STEP 2 COMPLETE
+  STEP 3 COMPLETE
+  FINISHED
+  
+  Hints: 
+  - Call `step1` first.
+  - You cannot call `step2` until after `step1` has "finished", similarly, you cannot call `step3` until `step2` has "finished".
+  - You must console.log the last line of the output, `FINISHED`, after `step3` has "finished".
+  */
+
+step1(function () {
+  step2(function () {
+    step3(function () {
+      console.log("FINISHED");
+    });
+  });
+});
