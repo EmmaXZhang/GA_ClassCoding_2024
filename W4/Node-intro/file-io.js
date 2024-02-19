584ba0ad5f734367abdfcff884a91f244950c730
EmmaZhang <emmazxd@gmail.com>
2024-02-19 12:27:16 +1100

    readFile() added

--- W4/Node-intro/file-io.js
+++ W4/Node-intro/file-io.js
@@ -1,9 +1,14 @@
 //recreate "cat" in js
 
-//old way to impor the fielsystem standard library
+//old way to impor the fielsystem standard library,error first pattern
 const fs = require("fs");
 
-//error first pattern, many asynchronous Node JS function follow this
+//many asynchronous Node JS function follow this
 fs.readFile("simpsons.txt", "utf-8", function (error, data) {
+  //error first parameter, dealing with error first
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log(data);
 });
