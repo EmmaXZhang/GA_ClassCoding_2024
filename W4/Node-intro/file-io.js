67307d7a14db48fb0843dda6df22c323e0945550
EmmaZhang <emmazxd@gmail.com>
2024-02-19 12:23:04 +1100

    node.js commit

--- W4/Node-intro/file-io.js
+++ W4/Node-intro/file-io.js
@@ -1,3 +1,9 @@
 //recreate "cat" in js
 
-//
+//old way to impor the fielsystem standard library
+const fs = require("fs");
+
+//error first pattern, many asynchronous Node JS function follow this
+fs.readFile("simpsons.txt", "utf-8", function (error, data) {
+  console.log(data);
+});
