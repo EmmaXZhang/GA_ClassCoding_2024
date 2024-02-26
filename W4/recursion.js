6d6dfca4677a17a4348adddf5ab8e106b5f3feb5
EmmaZhang <emmazxd@gmail.com>
2024-02-27 10:46:07 +1100

    factorial number recursion

---
+++ W4/recursion.js
@@ -0,0 +1,36 @@
+//iteration
+
+const countdownIterative = (limit = 10) => {
+  while (limit >= 0) {
+    console.log(limit);
+    limit -= 1;
+  }
+  console.log("blast off");
+};
+
+countdownIterative();
+
+//recursive
+const countdownRecursive = (limit = 10) => {
+  //if statement -> Base Case reached? -> finish
+  if (limit < 0) {
+    console.log("blast off");
+  } else {
+    //print current number
+    console.log(limit);
+    //call the function again BUT one step closer to the base case
+    countdownRecursive(limit - 1);
+  }
+};
+
+//factorial
+// 5 != 5*4*3*2;
+
+function factorial(num) {
+  let result = 1;
+  if (num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
