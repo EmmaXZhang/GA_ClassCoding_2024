6cd368a598f2a014c503a6b3482a68f98483a176
EmmaZhang <emmazxd@gmail.com>
2024-03-11 14:49:03 +1100

    add zoonIn function to img

--- W5/social-network/src/components/Profile.jsx
+++ W5/social-network/src/components/Profile.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
 // const Profile=(props)=>{
 //     return (
@@ -8,13 +10,25 @@
 // }
 
 const Profile = ({ name, motto, age, pic }) => {
+  const [height, setHeight] = useState(100);
+
+  const _zoonIn = () => {
+    setHeight(height + 10);
+  };
+
   return (
     <div>
       <h2>{name}</h2>
       <h3>{motto}</h3>
       <h3>{age}</h3>
+
+      <div>
+        <button onClick={_zoonIn}>+</button>
+        <button>-</button>
+      </div>
+
       <p>
-        <img src={pic} alt="" />
+        <img src={pic} alt={name} height={height} />
       </p>
     </div>
   );
