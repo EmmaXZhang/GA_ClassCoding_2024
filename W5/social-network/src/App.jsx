112c9ddbf91d12d5945b3a53cd00a8b1c878bdb9
EmmaZhang <emmazxd@gmail.com>
2024-03-12 10:36:14 +1100

    add greeting cadd omponent

--- W5/social-network/src/App.jsx
+++ W5/social-network/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import Profile from "./components/Profile";
+import Greeting from "./components/Greeting";
 
 function App() {
   return (
     <>
+      <h1>Social Network</h1>
+
+      <Greeting user="Bjorn" />
       <Profile
         name="Groucho"
         age="44"
---
+++ W5/social-network/src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+//import stuff
+
+//define a functional component
+const Greeting = ({ user }) => {
+  return <h2>Greetings,{user}</h2>;
+};
+
+//export the thing
+export default Greeting;
