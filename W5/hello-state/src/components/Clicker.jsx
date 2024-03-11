14d32185b494b7ac5c89abe55d745c5a7344b7b0
EmmaZhang <emmazxd@gmail.com>
2024-03-11 12:26:52 +1100

    add Clicker component

---
+++ W5/hello-state/src/components/Clicker.jsx
@@ -0,0 +1,8 @@
+const Clicker = () => {
+  // "_" -> indicate event handler
+  const _incrementCount = () => {};
+
+  return <button onClick={_incrementCount}>0 clicks so far</button>;
+};
+
+export default Clicker;
