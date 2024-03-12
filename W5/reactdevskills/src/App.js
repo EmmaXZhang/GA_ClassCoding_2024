316ea729049c6657e14a7081ef2e8da44c891f52
EmmaZhang <emmazxd@gmail.com>
2024-03-12 13:42:26 +1100

    add skills state

--- W5/reactdevskills/src/App.js
+++ W5/reactdevskills/src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import SkillList from "./components/SkillList";
 import NewSkillForm from "./components/NewSkillForm";
-
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
+import { useState } from "react";
 
 function App() {
+  const [skills, setSkills] = useState([
+    { name: "HTML", level: 5 },
+    { name: "CSS", level: 3 },
+    { name: "JavaScript", level: 4 },
+    { name: "Python", level: 2 },
+  ]);
+
   return (
     <div className="App">
       <h1 className="teal-text">React Dev Skills</h1>
