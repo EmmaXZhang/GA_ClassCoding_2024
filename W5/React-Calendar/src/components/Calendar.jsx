ebd5f05706cb4620f0c16a62b4dfc0783f7ad834
EmmaZhang <emmazxd@gmail.com>
2024-03-14 16:43:03 +1100

    fixed handleCellClick bug

--- W5/React-Calendar/src/components/Calendar.jsx
+++ W5/React-Calendar/src/components/Calendar.jsx
@@ -1,12 +1,24 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
 export default function Calendar({ days, dates }) {
   const calendar = [];
+  const [cellColor, setCellColor] = useState(Array(28).fill("orange"));
 
   const [select, setSelect] = useState(null);
+  const [colorSelect, setColorSelect] = useState(null);
+
+  const colorPicker = ["green", "yellow", "blue", "red"];
 
   function handleSelect(index) {
-    setSelect(index);
+    setSelect(index); // get color index
+    setColorSelect(colorPicker[index]); //get color selected
+  }
+
+  function handleCellClick(index) {
+    const updatedCellColor = [...cellColor];
+    updatedCellColor[index] = colorSelect;
+    setCellColor(updatedCellColor);
   }
 
   for (let i = 0; i < 28; i++) {
@@ -51,7 +63,12 @@ export default function Calendar({ days, dates }) {
 
       <div className="calendar">
         {calendar.map((cell, index) => (
-          <div key={index} className="cell">
+          <div
+            key={index}
+            className="cell"
+            style={{ backgroundColor: cellColor[index] }}
+            onClick={() => handleCellClick(index)}
+          >
             <p>{cell.day}</p>
             <p>{cell.date}</p>
           </div>
