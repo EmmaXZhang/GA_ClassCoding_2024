4a757640edbc94448d2507868aa349ccf9c7349e
EmmaZhang <emmazxd@gmail.com>
2024-03-18 10:46:43 +1100

    finish numberwang component

--- numberwang/src/components/NumberWang.jsx
+++ numberwang/src/components/NumberWang.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+
 export default function NumberWang() {
+  // state
+  const [trivia, setTrivia] = useState([]);
+
+  // array-> use .map to render
+
+  // async-await
+  const _fetchFact = async () => {
+    const res = await fetch("http://numbersapi.com/random/trivia?json").then(
+      (res) => res.json()
+    );
+
+    //create copy of trivia and add new object .text
+    const updatedTrivia = [...trivia, res.text];
+    //update trivia
+    setTrivia(updatedTrivia);
+  };
+
+  // event listener to get trivia
+  //   const _fetchFact = () => {
+  //     fetch("http://numbersapi.com/random/trivia?json")
+  //       .then((res) => res.json())
+  //       .then((res) => {
+  //         //the readabkeStream is js object
+  //         setTrivia(res.text);
+  //       });
+  //   };
+
   return (
     <div>
-      <button>Number Wang</button>
-      <p>Number wang is comming soon</p>
+      <button onClick={_fetchFact}>Number Wang</button>
+      {trivia.map((t) => (
+        <p key={t}>{t}</p>
+      ))}
     </div>
   );
 }
