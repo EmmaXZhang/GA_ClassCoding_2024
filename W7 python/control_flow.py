c1af9843347777385375e8c9ef0a285f364ce7f3
EmmaZhang <emmazxd@gmail.com>
2024-04-02 11:48:33 +1100

    conditional display python

---
+++ W7 python/control_flow.py
@@ -0,0 +1,20 @@
+# floor = "sticky"
+# walls = "clean"
+
+# if floor == "sticky":
+#     print("clean the floor, it is sticky")
+# if walls == "sticky":
+#     print("clean the wall")
+# else:
+#     print("the walls are clean")
+
+# input=input('input something: ')
+
+# if input=='green':
+#     print('Go!')
+# elif input=='yellow':
+#     print('slow down')
+# elif input=='red':
+#     print('stop')
+# else: print('bogus')
+
