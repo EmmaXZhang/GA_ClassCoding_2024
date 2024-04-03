cddffe275838f85e3eec3bedcc37ba73419ce80d
EmmaZhang <emmazxd@gmail.com>
2024-04-03 11:17:08 +1100

    Python list practice

--- W7 python/python_container.py
+++ W7 python/python_container.py
@@ -1,15 +1,33 @@
 
-student={
-    'name':'Maria',
-    'course':'SEI',
-    'current_week':7
-}
+# student={
+#     'name':'Maria',
+#     'course':'SEI',
+#     'current_week':7
+# }
 
-name = student['name']
+# name = student['name']
 
-for item_tuple in student.items():
-    print(f"key={item_tuple[0]} / value={item_tuple[1]}")
+# for item_tuple in student.items():
+#     print(f"key={item_tuple[0]} / value={item_tuple[1]}")
 
-# unpack tuples = destructure each property
-for key,value in student.items():
-    print(f"key={key} / value={value}")
+# # unpack tuples = destructure each property
+# for key,value in student.items():
+#     print(f"key={key} / value={value}")
+
+
+
+
+scores = [
+   {
+     'name': 'Bob',
+     'points': 25  # points the player scored
+   }
+ ]
+
+scores.append({
+    'name':'Mic',
+    'points': 30
+})
+
+for dic in scores:
+    print(f"{dic['name']} scored {dic['points']} points")
