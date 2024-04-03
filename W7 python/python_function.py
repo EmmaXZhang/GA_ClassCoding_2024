29b38f89f4fccc0f7fef01239a716f2befc73865
EmmaZhang <emmazxd@gmail.com>
2024-04-03 16:20:52 +1100

    ex2

--- W7 python/python_function.py
+++ W7 python/python_function.py
@@ -5,7 +5,7 @@ def dev_skills(dev_name, **kwargs):
 
 print(dev_skills('Jackie', HTML=5, CSS=3, JavaScript=4, Python=2))
 
-
+#ex1
 def sum_to(n):
   total=0
   for num in range(1,n+1):
@@ -13,3 +13,14 @@ def sum_to(n):
   return total
 
 print(sum_to(6))
+
+
+#ex2
+def largest(list):
+  largest = list[0]
+  for num in list:
+    if (num > largest):
+      largest = num
+
+  return largest
+print(largest([1, 2, 3, 4, 0]))
