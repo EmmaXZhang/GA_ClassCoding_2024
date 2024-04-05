3c5248ad91e34ae8219b20c4d0d0572cd54954c0
EmmaZhang <emmazxd@gmail.com>
2024-04-05 15:47:28 +1100

    add admin and more function on views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,20 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
-
 # Create your views here.
 def index(request):
     #interact with model
-
-
     #send back a response
-
     return HttpResponse("hellow world.")
+
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request,question_id):
+    response = "you're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(requset,question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
