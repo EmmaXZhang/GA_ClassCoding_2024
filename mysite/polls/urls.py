5e56e7803dd31eed18eef000b0d274458cf6181f
EmmaZhang <emmazxd@gmail.com>
2024-04-05 20:47:53 +1100

    finish part1-4 for django

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -11,13 +11,13 @@ app_name = "polls"
 
 urlpatterns =[
     # localhost:8000/polls/  -> define on entire url file
-    path("",views.index, name="index"),
+    path("",views.IndexView.as_view(), name="index"),
 
     # localhost:8000/polls/5/
-    path("<int:question_id>/",views.detail, name="detail"),
+    path("<int:pk>/",views.DetailView.as_view(), name="detail"),
 
     # localhost:8000/polls/5/results 
-    path("<int:question_id>/results",views.results, name="results"),
+    path("<int:pk>/results",views.ResultsView.as_view(), name="results"),
 
     # localhost:8000/polls/5/vote/
     path("<int:question_id>/vote",views.vote, name="vote"),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,29 +1,68 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
-from django.shortcuts import render,get_list_or_404
-from django.http import Http404
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question,Choice
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    #interact with model
-    # latest 5 poll questions
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    #send back a response via template
-    context = {"latest_question_list": latest_question_list}
-    return render(request,"polls/index.html",context)
+# def index(request):
+#     #interact with model
+#     # latest 5 poll questions
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     #send back a response via template
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request,"polls/index.html",context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = "latest_question_list"
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
+# def detail(request,question_id):
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request, "polls/detail.html",{"question":question})
 
-def detail(request,question_id):
-    question = get_list_or_404(Question,pk=question_id)
-    return render(request, "polls/detail.html",{"question":question})
+class DetailView(generic.DetailView):
+    model= Question
+    template_name='polls/detail.html'
     
 
-def results(request,question_id):
-    response = "you're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+# def results(request,question_id):
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,"polls/results.html",{"question":question})
 
-def vote(requset,question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+class ResultsView(generic.DetailView):
+    model=Question
+    template_name='polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST is a dictionary-like object that lets you access submitted data by key name -> choice's value
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        # F() object represents the value of a model field (votes) -> increate vote+1
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        # use reverse() to avoid hardcode url: "/polls/3/results/"
+        # where the 3 is the value of question.id. 
+        # This redirected URL will then call the 'results' view to display the final page.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
