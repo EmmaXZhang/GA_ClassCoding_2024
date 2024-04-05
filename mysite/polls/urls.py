8e629f2405711ab71162c17cc61c90d123bb0202
EmmaZhang <emmazxd@gmail.com>
2024-04-05 16:02:40 +1100

    add code on views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,6 +9,15 @@ from . import views
 
 urlpatterns =[
     # localhost:8000/polls/  -> define on entire url file
-    path("",views.index, name="index")
+    path("",views.index, name="index"),
+
+    # localhost:8000/polls/5/
+    path("<int:question_id>/",views.detail, name="detail"),
+
+    # localhost:8000/polls/5/results 
+    path("<int:question_id>/results",views.results, name="results"),
+
+    # localhost:8000/polls/5/vote/
+    path("<int:question_id>/vote",views.vote, name="vote"),
 ]
 
